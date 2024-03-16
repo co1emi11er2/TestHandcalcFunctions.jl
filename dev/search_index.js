4ad6e5f5343e504be07960082c0cd7e034f29cbe
Documenter.jl <documenter@juliadocs.github.io>
2024-03-16 16:40:41 +0000

    build based on e2921ed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestHandcalcFunctions","category":"page"},{"location":"#TestHandcalcFunctions","page":"Home","title":"TestHandcalcFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestHandcalcFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TestHandcalcFunctions]","category":"page"}]
+}
